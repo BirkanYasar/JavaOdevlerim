ee126a833e2156435ad2dfa41470ffcbed63f3ab
BirkanYasar <94866363+BirkanYasar@users.noreply.github.com>
2021-12-05 16:35:29 +0300

    4 ve 5'in kuvvetlerini ekrana yazdıran program
    Java döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran program

--- DongulerOdev.java
+++ DongulerOdev.java
@@ -1,36 +1,23 @@
 import java.util.Scanner;
 
-public class DongulerOdev {
+public class DongulerOdev2 {
 
 	public static void main(String[] args) {
+		/*Java döngüler ile girilen sayýya kadar olan 4 ve 5'in kuvvetlerini ekrana 
+		 * yazdýran programý yazýyoruz.*/
 
-		/*Ödev
-Java döngüler ile tek bir sayý girilene kadar 
-kullanýcýdan giriþleri kabul eden ve girilen deðerlerden
-çift ve 4'ün katlarý olan sayýlarý toplayýp ekrana basan 
-programý yazýyoruz.*/
-		
-		int n;
-		int total = 0;
 		Scanner scan = new Scanner(System.in);
 		
+		int n;
+		System.out.println("Sýnýr sayýsýný giriniz :");
+		n =scan.nextInt();
 		
-		do {
-			System.out.println("Bir sayý giriniz :");
-			n = scan.nextInt();
-			
-				if(n % 4 == 0) {
-					total += n;
-				}
-		}while (n % 2 == 0);
-		System.out.println("Program bitti!\n Toplam :" + total);
-					
-					
-				}
-
+		for(int i =20; i<=n; i+=20) {
+			System.out.println(i);
 		}
-		
-			
+
+	}
+}
 			
 			
 		
