37e85a4e5daf7f463baadaf5fea9f63369f5db32
BirkanYasar <birkanyasar.366@gmail.com>
2022-01-06 11:15:55 +0300

    First Commit.

---
+++ BMICalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Please enter your height (in meters) :");
+		double a = scan.nextDouble();
+		System.out.print("Please enter your weight (in kilograms) :");
+		double b = scan.nextDouble();
+		
+		double result = (b /(a*a));
+		System.out.println("BMI :"+ result);
+	}
+
+}//BirkanYasar
